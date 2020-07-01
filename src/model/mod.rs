ea57d8437cdb81065fce374f3fe19eb9a11fc064
Arthur E. Jones <PartyLich@gmail.com>
2020-07-01 13:17:09 -0500

    feat: add TrackFile model

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -6,3 +6,22 @@ enum FileType {
     Artwork,
     Track,
 }
+
+#[derive(Debug, PartialEq)]
+pub struct TrackFile {
+    pub bytes_received: usize,
+    pub downloaded: bool,
+    pub size: usize,
+    pub url: String,
+}
+
+impl TrackFile {
+    pub fn new(url: String, bytes_received: usize, size: usize) -> Self {
+        Self {
+            url,
+            bytes_received,
+            size,
+            downloaded: false,
+        }
+    }
+}
