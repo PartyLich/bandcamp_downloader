df2e151dda9068e4e152406a78326e8ffcdb5567
Arthur E. Jones <PartyLich@gmail.com>
2021-09-15 13:55:41 -0500

    fix: update helper::get_album_data for new html
    Rendered bandcamp markup has changed, necessitating new parsing.

--- src/helper.rs
+++ src/helper.rs
@@ -7,7 +7,7 @@ use crate::{
     error::Error,
     model::{Album, JsonAlbum},
     ui::{LogLevel, Message},
-    Result, ALBUM_RE, BAND_RE,
+    Result, ALBUM_RE, BAND_RE, HTML_QUOTE_RE,
 };
 
 fn log_channel<T: ToString>(mut sender: mpsc::Sender<Message>, level: LogLevel, msg: T) {
@@ -32,26 +32,15 @@ pub fn log_error<T: ToString>(sender: mpsc::Sender<Message>, msg: T) {
 fn get_album_data(raw_html: &str) -> Result<String> {
     lazy_static! {
         static ref ALBUM_DATA_RE: Regex =
-            regex::Regex::new(r"(?s)var TralbumData = (?P<data>\{.*?\});").unwrap();
-        static ref COMMENTS_RE: Regex = Regex::new(r"(?m)^\s*?//.*$").unwrap();
-        static ref TAIL_COMMENTS_RE: Regex = Regex::new(r"(?m)^(?:.*\s*?,)(\s//.*)$").unwrap();
-        static ref QUOTE_KEYS_RE: Regex =
-            Regex::new(r"(?m)^(?P<whitespace>\s*)(?P<key>\w*?):").unwrap();
+            Regex::new(r#"(?s)data-tralbum="(?P<data>\{.*?\})"\s?"#).unwrap();
     }
 
-    let album_data = ALBUM_DATA_RE
-        .captures(raw_html)
-        .ok_or(Error::NoAlbumData)?
-        .name("data")
-        .unwrap()
-        .as_str();
-
-    // using a js object that has unquoted keys, comments, etc
-    let album_data = COMMENTS_RE.replace_all(album_data, "");
-    let album_data = TAIL_COMMENTS_RE.replace_all(&album_data, "");
-    let album_data = QUOTE_KEYS_RE.replace_all(&album_data, r#"${whitespace}"${key}":"#);
-
-    Ok(album_data.to_string())
+    let album_data = HTML_QUOTE_RE.replace_all(raw_html, "\"");
+    ALBUM_DATA_RE
+        .captures(&album_data)
+        .and_then(|captures| captures.name("data"))
+        .map(|name| name.as_str().into())
+        .ok_or(Error::NoAlbumData)
 }
 
 // We're pulling from a javascript object literal, so we need to turn it into valid JSON before we
