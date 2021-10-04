ed3bfbf2f4642a216bd5402e5d2d4a95a9e68854
Arthur E. Jones <PartyLich@gmail.com>
2021-10-03 20:04:21 -0500

    feat: update settings on SettingsChanged messages

--- src/ui/iced/app.rs
+++ src/ui/iced/app.rs
@@ -6,7 +6,7 @@ use tokio::sync::Mutex;
 
 use super::{
     components::{main_view, settings_view, Entry, EntryMessage},
-    subscription, Message,
+    subscription, Message, SettingType,
 };
 use crate::{core::DownloadService, helper::log_info, settings::UserSettings, ui};
 
@@ -211,7 +211,6 @@ impl Application for App {
                 });
             }
             Message::SetSaveDir => {}
-            Message::SettingsChanged(..) => {}
             Message::Settings(message) => {
                 self.ui_state.settings.update(message);
             }
@@ -221,6 +220,32 @@ impl Application for App {
             Message::ThemeChanged(theme) => {
                 // println!("theme selected: {}", theme);
             }
+            Message::SettingsChanged(setting) => match setting {
+                SettingType::Language(language) => {
+                    // println!("Language selected: {}", language);
+                }
+                SettingType::Theme(theme) => {
+                    // println!("theme selected: {}", theme);
+                }
+                SettingType::SaveDir(value) => update_setting!(downloads_path, value),
+                SettingType::FilenameFormat(value) => update_setting!(file_name_format, value),
+                SettingType::Discography(value) => {
+                    update_setting!(download_artist_discography, value)
+                }
+                SettingType::ArtInFolder(value) => {
+                    update_setting!(save_cover_art_in_folder, value)
+                }
+                SettingType::ArtInTags(value) => update_setting!(save_cover_art_in_tags, value),
+                SettingType::ModifyTags(value) => update_setting!(modify_tags, value),
+                SettingType::TagYear(value) => update_setting!(tag_year, value),
+                SettingType::TagAlbumArtist(value) => update_setting!(tag_album_artist, value),
+                SettingType::TagAlbumTitle(value) => update_setting!(tag_album_title, value),
+                SettingType::TagArtist(value) => update_setting!(tag_artist, value),
+                SettingType::TagComments(value) => update_setting!(tag_comments, value),
+                SettingType::TagLyrics(value) => update_setting!(tag_lyrics, value),
+                SettingType::TagTrackNumber(value) => update_setting!(tag_track_number, value),
+                SettingType::TagTrackTitle(value) => update_setting!(tag_track_title, value),
+            },
         }
         Command::none()
     }
