faa3da6393752281a749443d9780d6aed078beb4
Arthur E. Jones <PartyLich@gmail.com>
2021-10-03 19:55:45 -0500

    feat: show pick lists for id3 tag settings

--- src/ui/iced/components/settings_view/naming.rs
+++ src/ui/iced/components/settings_view/naming.rs
@@ -4,7 +4,7 @@ use iced::{pick_list, text_input, Align, Column, Element, Length, PickList, Row,
 use crate::core::EditAction;
 use crate::settings::UserSettings;
 use crate::ui::{
-    iced::{components, Message},
+    iced::{components, Message, SettingType},
     IntlString,
 };
 
@@ -48,6 +48,42 @@ impl State {
             .width(Length::FillPortion(3))
             .push(filename_format)
             .push(modify_tags_checkbox)
+            .push(album_title_picker(
+                &mut self.tag_album_title,
+                &settings.tag_album_title,
+                intl,
+            ))
+            .push(album_artist_picker(
+                &mut self.tag_album_artist,
+                &settings.tag_album_artist,
+                intl,
+            ))
+            .push(date_picker(&mut self.tag_date, &settings.tag_year, intl))
+            .push(artist_picker(
+                &mut self.tag_artist,
+                &settings.tag_artist,
+                intl,
+            ))
+            .push(track_number_picker(
+                &mut self.tag_track_number,
+                &settings.tag_track_number,
+                intl,
+            ))
+            .push(track_title_picker(
+                &mut self.tag_track_title,
+                &settings.tag_track_title,
+                intl,
+            ))
+            .push(lyrics_picker(
+                &mut self.tag_lyrics,
+                &settings.tag_lyrics,
+                intl,
+            ))
+            .push(comments_picker(
+                &mut self.tag_comments,
+                &settings.tag_comments,
+                intl,
+            ))
             // TODO: move to art view
             .push(art_in_folder_checkbox)
             .push(art_in_tags_checkbox)
@@ -91,3 +127,20 @@ macro_rules! edit_action_picker {
         }
     };
 }
+
+edit_action_picker!(
+    album_artist_picker,
+    SettingType::TagAlbumArtist,
+    album_artist
+);
+edit_action_picker!(album_title_picker, SettingType::TagAlbumTitle, album_title);
+edit_action_picker!(artist_picker, SettingType::TagArtist, artist);
+edit_action_picker!(comments_picker, SettingType::TagComments, comments);
+edit_action_picker!(lyrics_picker, SettingType::TagLyrics, lyrics);
+edit_action_picker!(
+    track_number_picker,
+    SettingType::TagTrackNumber,
+    track_number
+);
+edit_action_picker!(track_title_picker, SettingType::TagTrackTitle, track_title);
+edit_action_picker!(date_picker, SettingType::TagYear, album_date);
